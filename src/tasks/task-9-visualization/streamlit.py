4a6e0acb115b5b19b76b60370b878a755b33d460
elena-andreini <andreini.elena@gmail.com>
2022-07-28 11:50:57 +0200

    relative path for html file

--- src/tasks/task-9-visualization/streamlit.py
+++ src/tasks/task-9-visualization/streamlit.py
@@ -9,7 +9,7 @@ st.write("Long Covid in Italy")
 #df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 #st.write(df)
 
-topics_coherence_handle = open(r'C:\LongCovidData\long-covid-repo\trieste-italy-long-covid\src\tasks\task-7-descriptive_data_analysis\final LDA attempt\02_Create pretrained LDA Model on Q1 Data\lda_visualization_6_topics_Q1_data.html', 'r')
+topics_coherence_handle = open(r'..\task-7-descriptive_data_analysis\final LDA attempt\02_Create pretrained LDA Model on Q1 Data\lda_visualization_6_topics_Q1_data.html', 'r')
 topics_coherence = topics_coherence_handle.read()
 components.html(topics_coherence, width=500, height=500, scrolling=True)
 
