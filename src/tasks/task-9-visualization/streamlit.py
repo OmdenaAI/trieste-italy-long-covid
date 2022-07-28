48f60fb12dfbb495a89189deb1df9329d33b81dd
elena-andreini <andreini.elena@gmail.com>
2022-07-28 11:48:23 +0200

    Added stub for streamlit visualization

---
+++ src/tasks/task-9-visualization/streamlit.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+
+
+st.write("Long Covid in Italy")
+
+#df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
+#st.write(df)
+
+topics_coherence_handle = open(r'C:\LongCovidData\long-covid-repo\trieste-italy-long-covid\src\tasks\task-7-descriptive_data_analysis\final LDA attempt\02_Create pretrained LDA Model on Q1 Data\lda_visualization_6_topics_Q1_data.html', 'r')
+topics_coherence = topics_coherence_handle.read()
+components.html(topics_coherence, width=500, height=500, scrolling=True)
+
